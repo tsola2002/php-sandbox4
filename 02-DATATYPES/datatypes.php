5d023a3cc8220b7c8139db818a21e0f983d87b43
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2025-11-13 12:17:08 +0100

    DATATYPES

---
+++ 02-DATATYPES/datatypes.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dataypes</title>
+</head>
+<body>
+    <h1>Working With Data Types</h1>
+    <?php 
+        // CREATING A STRING DATATYPE
+        $name = "John Doe";
+
+        // CREATING AN INTEGER DATATYPE
+        $age = 25;
+
+        // CREATING A FLOAT DATATYPE
+        $pi = 3.14;
+
+        // CREATING A BOOLEAN DATATYPE
+        $positive = false;
+
+        echo $name . var_dump($name) . "<br>";
+
+        echo $age . var_dump($age) . "<br>";
+
+        echo $pi . var_dump($pi) . "<br>";
+
+        echo $positive . var_dump($positive) . "<br>";
+
+        //COMPLEX 
+    ?>
+ 
+</body>
+</html>
