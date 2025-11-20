1853dcfd3e521ad55efd8db42149b2f7f1b466d7
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2025-11-20 12:09:44 +0100

    parameters

---
+++ 06-FUNCTIONS/basic-function.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Creating a Basic Function</h1>
+
+    <?php 
+
+    // Defining a basic function
+    function greet() {
+        echo "Hello There";
+    }
+
+    // invoking the function
+    greet();
+    
+    ?>
+</body>
+</html>
---
+++ 06-FUNCTIONS/function-parameter.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Using Function Parameters</h1>
+
+    <?php 
+
+    // Defining a basic function with parameters
+    function add($a, $b) {
+        echo $a + $b;
+    }
+
+    // invoking the function
+    add(2, 2);
+    
+    ?>
+</body>
+</html>
