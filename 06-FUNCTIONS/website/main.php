89e1fad94122f600379fd89daa2176b13ad4a652
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2025-11-21 12:51:53 +0100

    TEMPLATE

---
+++ 06-FUNCTIONS/website/main.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website Template</title>
+    <style>
+        body { text-align: center;}
+        .header{ height: 400px;background-color: orangered;}
+        .main{ height: 400px; background-color: green; }
+        .footer{ height: 400px; background-color: yellow;}
+    </style>
+</head>
+<body>
+    <div class="header">
+        <h1>Main Website</h1>
+    </div>
+    <div class="main">
+        MAIN SECTION
+    </div>
+    <div class="footer">
+        FOOTER SECTION
+    </div>
+</body>
+</html>
