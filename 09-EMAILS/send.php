fdad15b73c1a224c4eb360a3844d4f0220933f8d
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2025-12-04 13:40:31 +0100

    config details

--- 09-EMAILS/send.php
+++ 09-EMAILS/send.php
@@ -1,12 +1,10 @@
 <?php 
 
     use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\Exception;
+
+    require 'vendor/autoload.php'; 
 
 
-    require 'phpmailer/src/Exception.php';
-    require 'phpmailer/src/PHPMailer.php';
-    require 'phpmailer/src/SMTP.php';
 
     if(isset($_POST['send'])){
 
