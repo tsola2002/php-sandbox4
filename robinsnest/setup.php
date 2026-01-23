91b5c88c73ae36cd12d231ae45fdf62bc27a79ba
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2026-01-23 12:15:22 +0100

    setup script

---
+++ robinsnest/setup.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Setting up database</h1>
+    
+    <h3>Setting up...</h3>
+
+    <?php 
+        require_once('functions.php');
+
+        createTable('members', 
+        'user VARCHAR(16),
+        pass VARCHAR(16),
+        INDEX(user(6))');
+
+        createTable('messages', 
+                   'id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                   auth VARCHAR(16),
+                   recip VARCHAR(16),
+                   pm CHAR(1),
+                   time INT UNSIGNED,
+                   message VARCHAR(4096),
+                   INDEX(auth(6)),
+                   INDEX(recip(6))');
+
+        createTable('friends',
+                'user VARCHAR(16),
+                friend VARCHAR(16),
+                INDEX(user(6)),
+                INDEX(friend(6))');
+
+        createTable('profiles',
+                    'user VARCHAR(16),
+                    text VARCHAR(4096),
+                    INDEX(user(6))');
+    ?>
+
+    <br>...done
+</body>
+</html>
